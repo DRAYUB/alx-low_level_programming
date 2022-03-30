9082df3114b3597c162342d553891afa0c7b39bd
DRAYUB <ayoubaaboubaabdou@gmail.com>
2022-03-30 23:31:31 +0100

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * print_alphabet - print_alphabet
+ *
+ * Return:void
+
+ */
+int main(void)
+{
+
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+}
