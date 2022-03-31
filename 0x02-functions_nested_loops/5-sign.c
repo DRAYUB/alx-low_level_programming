b09f01d6530dfa04a700ea0b0b9daf45ec1c6a33
DRAYUB <ayoubaaboubaabdou@gmail.com>
2022-03-31 01:17:55 +0100

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+
+ #include "stdio.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+if (n == (1)){
+    printf(" + \n");
+}
+ if (n ==(0)){
+    printf("0\n");
+}
+if (n == (-1)){
+    printf("- \n");
+}
+    
+    return (0);
+}
