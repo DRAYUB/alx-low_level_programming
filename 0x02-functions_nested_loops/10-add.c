729318158332c59f6474c1fc19e00d073a25102f
DRAYUB <ayoubaaboubaabdou@gmail.com>
2022-03-31 01:53:04 +0100

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * add- print_alphabet
+ *@a : variable
+ *@b : variable
+ * Return: 0 or 1
+ */
+int main()
+{
+int a=2;
+int b=1;
+int result;
+result = a + b;
+
+printf("%d\n", a);
+printf("%d\n", b);
+printf("%d\n", result);
+
+return (result);
+}
