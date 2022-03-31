3d768097f09910bc1c7736d44b1f830394d35798
DRAYUB <ayoubaaboubaabdou@gmail.com>
2022-03-31 01:23:25 +0100

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+/**
+ * _abs- print_alphabet
+ *@n : the character
+ * Return: 0 or 1
+ */
+int main()
+{
+    int n;
+if (n >= 0)
+{
+return (n);
+}
+else if (n != 0 && n < 0)
+{
+return (-n);
+}
+return (0);
+}
