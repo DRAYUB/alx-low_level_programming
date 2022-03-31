8c1f7b361c430531147e423e75c6915946e1eb3a
DRAYUB <ayoubaaboubaabdou@gmail.com>
2022-03-31 01:57:31 +0100

    d

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * print_to_98- print_alphabet
+ *@n: variable
+ * Return: 0 or 1
+ */
+int main()
+{
+    int n;
+if (n <= 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+n++;
+if (n < 99)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+
+else if (n >= 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+n--;
+
+}
+}
+
+}
